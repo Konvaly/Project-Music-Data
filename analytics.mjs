d49da691c4f657848f6143b42e48e2116232ead7
Konvaly <konvaly@gmail.com>
2026-02-24 23:46:50 +0000

    feat: implement most listened song by count

--- analytics.mjs
+++ analytics.mjs
@@ -1,5 +1,38 @@
+import { getSong, getListenEvents } from "./data.mjs";
+
 export function getMostListenedSongByCount(userId) {
-  return null;
+  const events = getListenEvents(userId);
+
+  if (events.length === 0) return null;
+
+  const counts = {};
+
+  for (const event of events) {
+    const songId = event.song_id;
+
+    if (!counts[songId]) {
+      counts[songId] = 0;
+    }
+
+    counts[songId]++;
+  }
+
+  let maxSongId = null;
+  let maxCount = 0;
+
+  for (const songId in counts) {
+    if (counts[songId] > maxCount) {
+      maxCount = counts[songId];
+      maxSongId = songId;
+    }
+  }
+
+  const song = getSong(maxSongId);
+
+  return {
+    label: "Most listened song (count)",
+    value: `${song.artist} - ${song.title}`,
+  };
 }
 
 export function getMostListenedSongByTime(userId) {
